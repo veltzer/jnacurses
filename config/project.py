a8a5a229c39339c1e1e04fabc67baa7b47c0c530
Mark Veltzer <mark.veltzer@gmail.com>
2022-05-18 13:51:00 +0300

    new config

--- config/project.py
+++ config/project.py
@@ -1,4 +1,1 @@
-project_name = "jnacurses"
-project_long_description = "curses implementation for Java"
-project_year_started = 2010
-project_description = "curses implementation for Java"
+description_short = "curses implementation for Java"
